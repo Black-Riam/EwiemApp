fe5658a53f26502718102cda83ca8fa5363ed587
bright <obololastkiller@gmail.com>
2023-04-18 13:25:17 +0000

    Api updated

--- assets/js/api.js
+++ assets/js/api.js
@@ -4,3 +4,39 @@
  * @copyright nharnah 2023 All rights reserved
  * @author nharnah
  */
+
+"use strict";
+
+const api_key = "02e903b501cd64c535f918679a2dc26a";
+
+/**
+ * Fetch data from server
+ * @param {string} URL API url
+ * @param {Function} callback callback
+ */
+export const fetchData = function (URL, callback) {
+  fetch(`${URL}&appid=${api_key}`)
+    .then((res) => res.json())
+    .then((data) => callback(data));
+};
+
+export const url = {
+  currentWeather(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`;
+  },
+  forecast(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`;
+  },
+  airPollution(lat, lon) {
+    return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`;
+  },
+  reverseGeo(lat, lon) {
+    return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`;
+  },
+  /**
+   * @param {string} query Search query e.g.: "London", "New York"
+   */
+  geo(query) {
+    return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`;
+  },
+};
