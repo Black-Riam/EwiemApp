8723cc18a7ce2c26198401fa2e23acf4c0e048bb
Mariam Frimpomaa Adusei <101450922+Blacklov@users.noreply.github.com>
2023-04-20 21:12:57 +0000

    API. Js updated

--- assets/js/api.js
+++ assets/js/api.js
@@ -36,7 +36,7 @@ export const url = {
   /**
    * @param {string} query Search query e.g.: "London", "New York"
    */
-  g(query) {
+  geo(query) {
     return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`;
   },
 };
